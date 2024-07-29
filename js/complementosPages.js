df3500a8ba0c4cd50fd21b4cc116feda343dfb51
Guillermo Vera <veraguillermo345@gmail.com>
2024-07-28 22:49:03 -0300

    cambiando reutilizados para pages

--- js/complementosPages.js
+++ js/complementosPages.js
@@ -1,10 +1,10 @@
 //////////////Complementos/////////////
 //Imagen logo
-let logoContainer = document.querySelectorAll('.logo-container');
-logoContainer.forEach(element => {
+let logoContainerPages = document.querySelectorAll('.logo-container-pages');
+logoContainerPages.forEach(element => {
     let imgElement = document.createElement('img');
     imgElement.className = 'logo-img';
-    imgElement.src = `assets/images/logo-sports.png`;
+    imgElement.src = `../assets/images/logo-sports.png`;
     imgElement.alt = `Ir a 'Home'`;
     imgElement.title= `Overcomer Your Limits`;
     element.appendChild(imgElement);
@@ -12,8 +12,8 @@ logoContainer.forEach(element => {
 
 //////Tarjeta de redes y contacto footer
 //containers de contacto y redes
-let contacto = document.querySelector('.footer-contacto');
-let redes = document.querySelector('.footer-redes');
+let contactoPages = document.querySelector('.footer-contacto-pages');
+let redesPages = document.querySelector('.footer-redes-pages');
 
 //tarjeta para reutilizar
 let tarjeta = (title,imgs) => {
@@ -45,19 +45,17 @@ let tarjeta = (title,imgs) => {
     return fragment;
 };
 
-//arreglo de imagenes para tarjeta contacto
-let arregloImgsContacto = [
-    {src: '/assets/images/mail.png', content: 'hola@oyl.com.ar'},
-    {src: '/assets/images/phone.png', content: '+54 341 597 0645'}
+let arregloImgsContactoPages = [
+    {src: '../assets/images/mail.png', content: 'hola@oyl.com.ar'},
+    {src: '../assets/images/phone.png', content: '+54 341 597 0645'}
 ];
-//arreglo de imagenes para tarjeta redes
-let arregloImgsRedes = [
-    {src: '/assets/images/instagram.png', content: 'OvercomeYourLimits'},
-    {src: '/assets/images/x.png', content: 'OvercomeYourLimits'}
+let arregloImgsRedesPages = [
+    {src: '../assets/images/instagram.png', content: 'OvercomeYourLimits'},
+    {src: '../assets/images/x.png', content: 'OvercomeYourLimits'}
 ]
 
-contacto.appendChild(tarjeta('Contacto',arregloImgsContacto));
-redes.appendChild(tarjeta('Redes',arregloImgsRedes))
+contactoPages.appendChild(tarjeta('Contacto',arregloImgsContactoPages));
+redesPages.appendChild(tarjeta('Redes',arregloImgsRedesPages))
 
 //Manejar header
 let carritoContador = document.querySelector('.carritoIdicador');
